e6f8781038568ab1b7ead8eb8a6e975ade45a8df
Alejandro Feijoo <alejandro.feijoo@udarealestate.com>
2023-07-04 20:46:57 +0200

    vh text to px

--- src/styles/textTypes.js
+++ src/styles/textTypes.js
@@ -3,54 +3,54 @@ import { css } from "styled-components";
 export const textTypes = {
 	h1Main: css`
 		font-weight: 300;
-		font-size: 22.6875vw;
+		font-size: 363px;
 	`,
 	h1Bold: css`
 		font-weight: 500;
-		font-size: 22.6875vw;
+		font-size: 363px;
 	`,
 	h1Medium: css`
 		font-weight: 300;
-		font-size: 16vw;
+		font-size: 256px;
 	`,
 	h1Reduced: css`
 		font-weight: 300;
-		font-size: 13.875vw;
+		font-size: 222px;
 	`,
 	h2Main: css`
 		font-weight: 500;
-		font-size: 16.875vw;
+		font-size: 270px;
 	`,
 	h2Gray: css`
 		font-weight: 500;
-		font-size: 16.875vw;
+		font-size: 270px;
 	`,
 	h3Main: css`
 		font-weight: 500;
-		font-size: 10vw;
+		font-size: 160px;
 	`,
 	h4Main: css`
 		font-weight: 300;
-		font-size: 10vw;
+		font-size: 160px;
 	`,
 	h5Main: css`
 		font-weight: 500;
-		font-size: 5vw;
+		font-size: 80px;
 	`,
 	h6Main: css`
 		font-weight: 500;
-		font-size: 1.625vw;
+		font-size: 26px;
 	`,
 	mainText: css`
 		font-weight: 400;
-		font-size: 2.5vw;
+		font-size: 40px;
 	`,
 	mainTitles: css`
 		font-weight: 300;
-		font-size: 3.125vw;
+		font-size: 50px;
 	`,
 	paragraph: css`
 		font-weight: 300;
-		font-size: 1.625vw;
+		font-size: 26px;
 	`,
 };
