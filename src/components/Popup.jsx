3aa926818290dba0cc8b892b271aad036d851a47
Alejandro Feijoo <alejandro.feijoo@udarealestate.com>
2023-07-07 20:30:48 +0200

    Popup Anim

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { useState } from "react";
-import styled from "styled-components";
+import { useState, useEffect } from "react";
+import styled, { css } from "styled-components";
 import { themes } from "../styles/themes";
 import { Button } from "./Button";
 import { useTheme } from "../styles/ThemeContext";
@@ -9,6 +9,34 @@ import { THEME } from "../utils/constants";
 import { paths } from "../router/paths";
 import { useNavigate } from "react-router-dom";
 import { colors } from "../styles/colors";
+
+const animationEnter = css`
+	animation: slide-in 0.5s forwards;
+	@keyframes slide-in {
+		0% {
+			transform: translateY(100%);
+			opacity: 0;
+		}
+		100% {
+			transform: translateY(0);
+			opacity: 1;
+		}
+	}
+`;
+
+const animationExit = css`
+	animation: slide-out 0.5s forwards;
+	@keyframes slide-out {
+		0% {
+			transform: translateY(0);
+			opacity: 1;
+		}
+		100% {
+			transform: translateY(100%);
+			opacity: 0;
+		}
+	}
+`;
 
 const PopupWrapper = styled.div`
 	${({ theme }) => themes[theme]}
@@ -24,6 +52,8 @@ const PopupWrapper = styled.div`
 	font-weight: 300;
 	display: flex;
 	align-items: center;
+
+	${(props) => (props.open ? animationEnter : animationExit)};
 `;
 
 const PopupContent = styled.div`
@@ -41,6 +71,7 @@ const ButtonsContent = styled.div`
 
 export default function Popup() {
 	const [isOpen, setIsOpen] = useState(true);
+	const [isRendered, setIsRendered] = useState(true);
 	const { theme } = useTheme();
 	const navigate = useNavigate();
 
@@ -52,9 +83,19 @@ export default function Popup() {
 		navigate(paths.cookies);
 	};
 
+	useEffect(() => {
+		if (!isOpen) {
+			const timer = setTimeout(() => {
+				setIsRendered(false);
+			}, 500);
+
+			return () => clearTimeout(timer);
+		}
+	}, [isOpen]);
+
 	return (
-		isOpen && (
-			<PopupWrapper theme={theme}>
+		isRendered && (
+			<PopupWrapper theme={theme} open={isOpen}>
 				<PopupContent>
 					<p>Hello there! This website uses cookies to provide a better browsing experience.</p>
 					<ButtonsContent>
